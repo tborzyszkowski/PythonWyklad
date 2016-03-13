ad7cf77c718bb81d3276079c9b341f4894538e70
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-03-13 10:09:13 +0100

    Max review

--- lab/Maksimum.py
+++ lab/Maksimum.py
@@ -1,6 +1,7 @@
 
-def max1(a ,b):
-    return a if a>b else b
+def max1(a, b):
+    return a if a > b else b
+
 
 def max2(*args, **kwargs):
     result = 0
@@ -8,16 +9,16 @@ def max2(*args, **kwargs):
         result = reduce(max1, args)
         if kwargs:
             result = reduce(max1, [result] + kwargs.values())
-    else: 
+    else:
         result = reduce(max1, kwargs.values())
-    return result 
+    return result
 
 print max2("Ala", "ma", "kota")
 print max2(1, 2, 3, 4.5)
 print max2(1, 2, 3, 4.5, x=8, y=9)
 print max2(a=-1, b=-2)
-kk = (1,2,3,4,5)
-dd = {'x':9, 'y':8}
+kk = (1, 2, 3, 4, 5)
+dd = {'x': 9, 'y': 8}
 print max2(*kk, **dd)
 
 # print max2(*("Ala ma kota".split()))
