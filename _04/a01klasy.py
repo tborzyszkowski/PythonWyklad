e2e6bb7670316b44e993840d4bfe55e4b9ac5911
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-03-20 17:37:09 +0100

    klasy

--- _04/a01klasy.py
+++ _04/a01klasy.py
@@ -4,7 +4,7 @@
 class NicNieRob:
     pass
 
-dir(NicNieRob)
+# print dir(NicNieRob)
 
 # przykladowa klasa bez inicjalizacji
 
@@ -15,56 +15,72 @@ class MyClass:
     i = 12345
 
     def f(self):
+        """Opis funkcji
+        """
         return 'hello world %d' % self.__class__.i
 
 x = MyClass()
 y = MyClass()
-print x.f()
-print x.__class__.i, y.__class__.i
+# print x.f()
+# print x.__class__.i, y.__class__.i
 # y.i = 54321
-y.__class__.i = 13579
-print x.__class__.i, y.__class__.i
-print x.f(), y.f()
+# # y.__class__.i = 13579
+# print x.__class__.i, y.__class__.i
+# print x.f(), y.f()
+# print y.i, x.i
 
 
-print x.__doc__
-print '-'*20
-
-# klasa z inicjalizacja stanu klasy
+# print x.__doc__
+# print x.f.__doc__
+# print '-'*20
+#
+# # klasa z inicjalizacja stanu klasy
 
 class MojaKlasa:
-    i = 1
-    def __init__(self, a = 10, b = 20):
+    i = 0
+
+    def __init__(self, a=10, b=20):
         self.__class__.i += 1
         self.a = a
         self.b = b
+
     def wypisz(self):
         print self.i, self.a, self.b
 
-y = MojaKlasa(30,40)
-y.wypisz()
+
+y = MojaKlasa(30, 40)
+# y.wypisz()
 z = MojaKlasa()
-z.wypisz()
-MojaKlasa(13).wypisz()
+# z.wypisz()
+# MojaKlasa(13).wypisz()
+# y.wypisz()
 
-# czym jest
-print MojaKlasa.wypisz
-# a czym
-print y.wypisz
-
-# z drugiej strony
-y.wypisz()
-# jest rownowazne
-MojaKlasa.wypisz(y)
-
-
+# # czym jest
+# print MojaKlasa.wypisz
+# # a czym
+# print y.wypisz
+#
+# # z drugiej strony
+# y.wypisz()
+# # jest rownowazne
+# MojaKlasa.wypisz(z)
+# nn = NicNieRob()
+# nn.i = 0
+# nn.a = 2
+# nn.b = 3
+# MojaKlasa.wypisz(nn)
+#
+#
 # kasujemy atrybuty obiektu
-#del y.i
-#del y.a
-#print y.a
+# del y.i
+# del y.__class__.i
+del y.a
+# print y.a
+print z.a
 
-# a teraz klasy
-#del MojaKlasa.i
-# metody tez mozemy kasowac
-#del MojaKlasa.wypisz
-# ale czy z obiektu tez mozna usuwac metody?
+#
+# # a teraz klasy
+del MojaKlasa.i
+# # metody tez mozemy kasowac
+del MojaKlasa.wypisz
+# # ale czy z obiektu tez mozna usuwac metody?
