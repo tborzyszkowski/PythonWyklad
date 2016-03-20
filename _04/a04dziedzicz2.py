3897a39c646d3b900822f8fc1c29a9ca91c6d8e3
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-03-20 18:20:40 +0100

    dziedziczenie 2

--- _04/a04dziedzicz2.py
+++ _04/a04dziedzicz2.py
@@ -1,31 +1,43 @@
 # przyklady dziedziczenia wielokrotnego
 
+
 class B1:
     i = 1
+
     def __init__(self):
         self.a = 10
         self.b = 20
+
     def f(self):
         return self.a + self.b
+
     def getI(self):
         return self.__class__.i
+
 
 class B2:
     i = -1
+
     def __init__(self):
         self.a = 100
         self.b = 200
+
     def f(self):
         return self.a * self.b
+
     def getI(self):
         return self.__class__.i
 
-class C(B2, B1):
+
+class C(B1, B2):
+
     def __init__(self):
-        B1.__init__(self)
         B2.__init__(self)
+        B1.__init__(self)
+
     def g(self):
         return self.f()
+
     def h(self):
         return self.getI()
 
