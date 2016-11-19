670ee077a92112f813e3eea3d7bcf123e2b3abd9
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-19 09:37:27 +0100

    fixes

--- lab/axbyc.py
+++ lab/axbyc.py
@@ -1,23 +1,26 @@
 '''
 ax + by + c =0
 '''
-a = 1
-b = 0
+a = 0
+b = 1
 c = 1
 
-x = ((a == 0) and [(c == 0 and ['duzo'] or ['nie ma'])[0]] or [-c/float(a)])[0]
+res = ('duzo' if c == 0 else 'nie ma') if a == 0 else -c/float(a)
+
+print res
+
+# x = ((a == 0) and [(c == 0 and ['duzo'] or ['nie ma'])[0]] or [-c/float(a)])[0]
 # <warunek> ? wy1 : wyr2
 # wyr1 if <warunek> else wyr2
-y = ('duzo' if c == 0 else 'nie ma') if a == 0 else -c/float(a)
-print x, y
-
-wynik = ""
-if a == 0:
-    if c == 0:
-        wynik = 'duzo'
-    else:
-        wynik = 'nie ma'
-else:
-    wynik = -c/float(a)
-
-print wynik
+# print x, y
+#
+# wynik = ""
+# if a == 0:
+#     if c == 0:
+#         wynik = 'duzo'
+#     else:
+#         wynik = 'nie ma'
+# else:
+#     wynik = -c/float(a)
+#
+# print wynik
