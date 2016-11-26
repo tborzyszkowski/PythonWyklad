502699b43dc0c97f15f764c4560999fb3da35827
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 09:35:34 +0100

    min

--- lab/instrukcje/minRandList.py
+++ lab/instrukcje/minRandList.py
@@ -2,5 +2,14 @@ import random
 
 a = range(0, 100)
 print len(a), a[:10]
-b = random.sample(a, 5)
+b = random.sample(a, 10)
 print b
+
+najmn = b[0]
+i = 0
+while i < len(b):
+    if najmn > b[i]:
+        najmn = b[i]
+    i += 1
+
+print "minimum:", najmn
