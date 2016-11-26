99795f36e6e47c9c4aaad16da406cfe14e866343
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 08:46:08 +0100

    ify

---
+++ lab/instrukcje/ify.py
@@ -0,0 +1,21 @@
+d1 = 26
+m1 = 11
+r1 = 2016
+d2 = 27
+m2 = 11
+r2 = 2016
+
+if r2 > r1:
+    print 'Data 2 większa niż Data 1'
+elif r2 < r1:
+    print 'Data 1 większa niż Data 2'
+elif m2 > m1:
+    print 'Data 2 większa niż Data 1'
+elif m2 < m1:
+    print 'Data 1 większa niż Data 2'
+elif d2 > d1:
+    print 'Data 2 większa niż Data 1'
+elif d2 < d1:
+    print 'Data 1 większa niż Data 1'
+else:
+    print 'Daty są sobie równe'
