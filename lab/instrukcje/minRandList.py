b605cc2c58e5d7279ef2d52e7c99c28866f96ef4
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 10:59:11 +0100

    min for

--- lab/instrukcje/minRandList.py
+++ lab/instrukcje/minRandList.py
@@ -2,20 +2,28 @@ import random
 from datetime import datetime
 
 a = range(0, 100000)
-#print len(a), a[:100]
+# print len(a), a[:100]
 b = random.sample(a, 50000)
 # print b
 
 t1 = datetime.now()
 najmn = 0
 najwi = 0
-i = 0
-while i < len(b):
+# i = 0
+# while i < len(b):
+#     if b[najmn] > b[i]:
+#         najmn = i
+#     if b[najwi] < b[i]:
+#         najwi = i
+#     i += 1
+najmn = 0
+najwi = 0
+for i in range(0, len(b)):
     if b[najmn] > b[i]:
         najmn = i
     if b[najwi] < b[i]:
         najwi = i
-    i += 1
+
 t2 = datetime.now()
 
 print "minimum:", b[najmn]
