159c5afadc799319b68c19b3874120a93f20ac86
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 09:00:07 +0100

    ify2

--- lab/instrukcje/ify.py
+++ lab/instrukcje/ify.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 d1 = 26
 m1 = 11
 r1 = 2016
@@ -19,3 +21,18 @@ elif d2 < d1:
     print 'Data 1 większa niż Data 1'
 else:
     print 'Daty są sobie równe'
+
+dd1 = (r1, m1, d1)
+dd2 = (r2, m2, d2)
+
+if dd1 < dd2:
+    print "Pierwsza"
+elif dd1 > dd2:
+    print "Druga"
+else:
+    print "Rowne"
+
+dl1 = r1 * 10000 + m1 * 100 + d1
+dl2 = r2 * 10000 + m2 * 100 + d2
+
+print dl1, dl2, "Pierwsza" if dl1 < dl2 else ("Druga" if dl1 > dl2 else "Rowne")
