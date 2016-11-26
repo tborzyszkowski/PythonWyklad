eebb144c52fc5c203186b8946d5b27ad1bb2c30b
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 12:02:04 +0100

    bubbles + sorted

--- lab/instrukcje/sortowanie.py
+++ lab/instrukcje/sortowanie.py
@@ -1,16 +1,22 @@
 import random
 from datetime import datetime
 
-N = 5000
+N = 10000
 a = range(0, 100000)
 b = random.sample(a, N)
 
 t1 = datetime.now()
 for i in range(0, N):
-    for k in range(i, N):
-        j = N - k + i - 1
+    for j in range(N-1, i, -1):
         if b[j-1] > b[j]:
             b[j-1], b[j] = b[j], b[j-1]
 t2 = datetime.now()
-print b[:N /10]
+# print b[:N / 10]
+print "time:   ", (t2 - t1).total_seconds() * 1000
+
+b = random.sample(a, N)
+t1 = datetime.now()
+sorted(b)
+t2 = datetime.now()
+#print b[:N / 10]
 print "time:   ", (t2 - t1).total_seconds() * 1000
