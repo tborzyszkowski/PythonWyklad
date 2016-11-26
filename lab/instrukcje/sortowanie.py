396dd12dfef0852e8637d5905fd78fa8189e3031
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-11-26 11:36:58 +0100

    bubble

---
+++ lab/instrukcje/sortowanie.py
@@ -0,0 +1,16 @@
+import random
+from datetime import datetime
+
+N = 5000
+a = range(0, 100000)
+b = random.sample(a, N)
+
+t1 = datetime.now()
+for i in range(0, N):
+    for k in range(i, N):
+        j = N - k + i - 1
+        if b[j-1] > b[j]:
+            b[j-1], b[j] = b[j], b[j-1]
+t2 = datetime.now()
+print b[:N /10]
+print "time:   ", (t2 - t1).total_seconds() * 1000
