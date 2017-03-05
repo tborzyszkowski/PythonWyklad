fa21f1e2327dcb1480f5a0770c13539f77ddf2cb
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-05 09:11:48 +0100

    format

--- lab/01_start/02_logika.py
+++ lab/01_start/02_logika.py
@@ -4,9 +4,12 @@ c = [0, 10]
 
 # w = a and b or c
 
-str_format = '%10s %10s %10s %10s'
-print str_format % ("a", "b", "c", "a and b or c")
+# str_format = '%10s %10s %10s %10s'
+str_format2 = '{:^10} {:^10} {:^10} {:^10}'
+# print str_format % ("a", "b", "c", "a and b or c")
+print str_format2.format("a", "b", "c", "a and b or c")
 for aa in a:
     for bb in b:
         for cc in c:
-            print str_format % (aa, bb, cc, aa and bb or cc)
+            # print str_format % (aa, bb, cc, aa and bb or cc)
+            print str_format2.format(aa, bb, cc, aa and bb or cc)
