495c017daf16a2ec2cb1759b000867a504d9af26
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-05 10:36:30 +0100

    matrix

---
+++ lab/01_start/06_macierzeDict.py
@@ -0,0 +1,23 @@
+from random import randint
+
+n = 1000
+m1 = {w: {k: randint(0,5) for k in range(n)} for w in range(n)}
+# m2 = {w: {k: (1 if w ==k else 0) for k in range(n)} for w in range(n)}
+m2 = {w: {k: randint(0,5) for k in range(n)} for w in range(n)}
+wyn = {w: {k: 0 for k in range(n)} for w in range(n)}
+
+for w in range(n):
+    for k in range(n):
+        for c in range(n):
+            wyn[w][k] += m1[w][c]*m2[c][k]
+
+for w in range(n):
+    print m1[w].values()
+print
+
+for w in range(n):
+    print m2[w].values()
+print
+
+for w in range(n):
+    print wyn[w].values()
