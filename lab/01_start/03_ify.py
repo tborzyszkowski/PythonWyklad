88a33546b6a5c9bd81da783a90a06da80a37418d
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-05 12:05:34 +0100

    and_or

--- lab/01_start/03_ify.py
+++ lab/01_start/03_ify.py
@@ -1,14 +1,19 @@
 aa = [None, {}, {'q': 12}]
 bb = [None, "", "Ala"]
 
-str_format2 = '{:^15} {:^15} {:^20} {:^25} {:^30}'
+str_format2 = '{:^15} {:^15} {:^20} {:^25} {:^30} {:^20}'
 print str_format2.format("a", "b",
             "a if a>b else b",
             "(a > b and [a] or [b])[0]",
-            "(a > b and {0: a} or {0: b})[0]")
+            "(a > b and {0: a} or {0: b})[0]",
+            "a > b and a or b")
+
+# (a > b ? a : b)
+
 for a in aa:
     for b in bb:
         print str_format2.format(a, b
                                 , a if a > b else b
                                 , (a > b and [a] or [b])[0]
-                                , (a > b and {0: a} or {0: b})[0])
+                                , (a > b and {0: a} or {0: b})[0]
+                                , a > b and a or b)
