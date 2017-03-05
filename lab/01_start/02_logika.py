611395e9de867ff232620351ad205c5674e00a43
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-05 11:53:08 +0100

    operatory

--- lab/01_start/02_logika.py
+++ lab/01_start/02_logika.py
@@ -1,15 +1,15 @@
-a = [{}, {'q': 12}]
-b = ["", "Ala"]
-c = [0, 10]
+aList = [{}, {'q': 12}]
+bList = ["", "Ala"]
+cList = [0, 10]
 
 # w = a and b or c
 
 # str_format = '%10s %10s %10s %10s'
-str_format2 = '{:^10} {:^10} {:^10} {:^10}'
-# print str_format % ("a", "b", "c", "a and b or c")
-print str_format2.format("a", "b", "c", "a and b or c")
-for aa in a:
-    for bb in b:
-        for cc in c:
-            # print str_format % (aa, bb, cc, aa and bb or cc)
-            print str_format2.format(aa, bb, cc, aa and bb or cc)
+str_format2 = '{:^10} {:^10} {:^10} {:^15} {:^15} {:^15}'
+# print str_format % ("a", "b", "c", "a and b or c","(a and b) or c","a and (b or c)")
+print str_format2.format("a", "b", "c", "a and b or c","(a and b) or c","a and (b or c)")
+for a in aList:
+    for b in bList:
+        for c in cList:
+            # print str_format % (a, b, c, a and b or c)
+            print str_format2.format(a, b, c, a and b or c, (a and b) or c, a and (b or c))
