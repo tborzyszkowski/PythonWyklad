ebb06f2354b048bf014f26403f6c6ac64c7b49d9
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-26 17:18:20 +0200

    aplihelper

--- _02_Funkcje/apihelper.py
+++ _02_Funkcje/apihelper.py
@@ -39,8 +39,7 @@ def info(object, spacing=10, collapse=1):
 	methodList = [e for e in dir(object) if callable(getattr(object, e))]
 	processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
 	print "\n".join(["%s %s" % (method.ljust(spacing),
-					  processFunc(str(getattr(object, method).__doc__)))
-					 for method in methodList])
+		processFunc(str(getattr(object, method).__doc__))) for method in methodList])
 
 # print __name__
 if __name__ == "__main__":
