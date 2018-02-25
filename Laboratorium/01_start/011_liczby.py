54c72f531ed9d69f5343441d2cfe6232b5e8bb44
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-02-25 17:41:10 +0100

    liczby

--- Laboratorium/01_start/011_liczby.py
+++ Laboratorium/01_start/011_liczby.py
@@ -2,7 +2,7 @@ import time
 import math
 
 t0 = time.time()
-x = 1234500 ** 67890
+x = 1234500 ** 123456
 t1 = time.time()
 xLen =len(str(x))
 t2 = time.time()
@@ -10,7 +10,7 @@ t2 = time.time()
 print "Time pow:", t1 - t0, "Time len:", t2 - t1, "Len:", xLen
 
 t0 = time.time()
-x = 1234500 ** 67890
+x = 1234500 ** 123456
 t1 = time.time()
 xLen=int(math.ceil(math.log(x,10)))
 
