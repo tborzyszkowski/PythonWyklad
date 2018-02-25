49b8a85cbd3c1f91381546b527e7bf7eff346921
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-02-26 00:44:34 +0100

    liczby unix/win time

--- Laboratorium/01_start/011_liczby.py
+++ Laboratorium/01_start/011_liczby.py
@@ -1,20 +1,28 @@
 import time
 import math
+import sys
 
-t0 = time.time()
-x = 12345 ** 1234567
-t1 = time.time()
-xLen = 0 #len(str(x))
-t2 = time.time()
+if sys.platform == 'win32':
+    # On Windows, the best timer is time.clock
+    default_timer = time.clock
+else:
+    # On most other platforms the best timer is time.time
+    default_timer = time.time
+
+t0 = default_timer()
+x = 12345 ** 123456
+t1 = default_timer()
+xLen = len(str(x))
+t2 = default_timer()
 
 print "Time pow:", t1 - t0, "Time len:", t2 - t1, "Len:", xLen
 
-t0 = time.time()
-x = 12345 ** 1234567
-t1 = time.time()
+t0 = default_timer()
+x = 12345 ** 123456
+t1 = default_timer()
 xLen=int(math.ceil(math.log(x,10)))
 
-t2 = time.time()
+t2 = default_timer()
 
 print "Time pow:", t1 - t0, "Time len:", t2 - t1, "Len:", xLen
 
