b8f4b7891abdd1d500b5246b680840a26d60b3dd
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-03-18 17:58:33 +0100

    lazy compare ??

---
+++ Laboratorium/02_instrukcje/porownania.py
@@ -0,0 +1,16 @@
+
+def f(p):
+    print p
+    return p
+
+def loop(x):
+    while True:
+        x += 1
+        if x % 10 == 0:
+            print "break"
+            break
+    return 0
+
+# (f(1), f(3), f(3)) < (f(1), f(2), loop(0))
+# [f(1), f(3), f(3)] < [f(1), f(2), loop(0)]
+{1: f(1), 2: f(3), 3: f(3)} < {1: f(1), 2: f(2), 3: loop(0)}
