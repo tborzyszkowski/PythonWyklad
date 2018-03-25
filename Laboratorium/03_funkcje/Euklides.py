4e48819ef83c0ecd1e4729ae2f28695be4b33c57
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-03-25 17:52:47 +0200

    euklides

--- Laboratorium/03_funkcje/Euklides.py
+++ Laboratorium/03_funkcje/Euklides.py
@@ -10,16 +10,7 @@ def is_prime(num):
             return False
     return True
 
-# a = int(raw_input('a = '))
-# b = int(raw_input('b = '))
-#
-# while a != b:
-#     if a > b:
-#         a -= b
-#     else:
-#         b -= a
-# else:
-#     print 'gcd = ', a
+
 def euklidesMod(a, b):
     if a <= b:
         return euklidesMod(b, a)
@@ -43,27 +34,12 @@ def euklides(a, b):
 def euklides2(*args):
     return reduce(euklidesMod, args)
 
-lista = [16, 4, 8]
-print euklides2(9, 6, 12, 99, 30)
+# lista = [16, 4, 8]
+# print euklides2(9, 6, 12, 99, 30)
 
 # print reduce(lambda x,y: x+y,[1,2,3])
 
 # print map(lambda x: x+1, [1])
-
-big1 = random.getrandbits(256)
-big2 = random.getrandbits(256)
-fst = 10000000000L
-snd = 10000010000L
-big3 = filter(is_prime, range(fst, snd))
-print len(big3)
-
-prime = 0
-if big3:
-    prime = big3[0]
-else:
-    prime = 997
-print big1, big2, prime
-
 
 if sys.platform == 'win32':
     # On Windows, the best timer is time.clock
@@ -73,7 +49,37 @@ else:
     default_timer = time.time
 
 
+
+#big1 = random.getrandbits(256)
+#big2 = random.getrandbits(256)
+fst = 10000000000L
+snd = 10000010000L
+
 start = default_timer()
-print euklidesMod(prime * prime, prime * prime * prime)
+
+big3 = filter(is_prime, range(fst, snd))
+
 stop = default_timer()
-print 'Time:', stop - start
+print 'Time is_prime:', stop - start
+
+print len(big3)
+
+prime = 0
+if big3:
+    prime = big3[0]
+else:
+    prime = 997
+print prime
+
+
+start = default_timer()
+for prime in big3:
+    print prime, euklidesMod(prime * prime, prime * prime * prime)
+stop = default_timer()
+print 'Time euklidesMod:', (stop - start) * 1000
+
+# start = default_timer()
+# for prime in big3:
+#     print euklides(prime * prime, prime * prime * prime)
+# stop = default_timer()
+# print 'Time euklides:', stop - start
