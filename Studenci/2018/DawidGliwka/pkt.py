ec22b5c009af7e56b5c7d05dd0fd5a838502a99b
Dawid Gliwka <dawid.gliwka@gmail.com>
2018-04-15 17:08:47 +0200

    zajęcia

--- Studenci/2018/DawidGliwka/pkt.py
+++ Studenci/2018/DawidGliwka/pkt.py
@@ -4,13 +4,13 @@ class Point:
         self.y = y
 
 class Rectangle:
-    def __init__(self, p1, p2)
+    def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-
+        self.area = abs(p1.x-p2.x)*abs(p1.y-p2.y)
 
 pkt  = Point(13, 14)
-pkt2 = Point(16, 23
-print(pkt)
+pkt2 = Point(16, 23)
 print(pkt.x, pkt.y)
 rect = Rectangle(pkt, pkt2)
+print(rect.area)
