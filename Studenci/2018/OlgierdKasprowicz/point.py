74bfb34370e7b309d0c531f141f73679a817a2df
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2018-04-15 14:43:51 +0200

    Coded point

---
+++ Studenci/2018/OlgierdKasprowicz/point.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from math import sqrt
+
+
+class Point():
+    def __init__(self, *coordinates):
+        self.coordinates = coordinates
+
+    def distance(self, other):
+        return sqrt(sum({(xi - xj) ** 2 for xi, xj in zip(self.coordinates, other.coordinates)}))
+     
+
+def main():
+    pass
+
+
+def test_same_points():
+    p = Point(1,1)
+    q = Point(1,1)
+
+    assert p.distance(q) == 0
+
+
+def test_different_points():
+    p = Point(1)
+    q = Point(0)
+
+    assert p.distance(q) == 1
+
+
+def test_alteration():
+    p = Point(1)
+    q = Point(0)
+
+    assert p.distance(q) == q.distance(p)
+    
+
+if __name__ == "__main__":
+    main()
