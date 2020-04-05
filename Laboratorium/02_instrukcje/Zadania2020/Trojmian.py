e41e61d3a9b4077b7dda7c911af693087ac93e92
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-04-05 14:37:23 +0200

    trojmian

--- Laboratorium/02_instrukcje/Zadania2020/Trojmian.py
+++ Laboratorium/02_instrukcje/Zadania2020/Trojmian.py
@@ -1,6 +1,14 @@
-#
-# result: { ile_pierwiastkow: n, pierwiastki: (0, ..., n-1) }
+
+
 def pierwiaski_trojmianu(a, b, c):
+    """
+    Wylicza pierwastki rownania kwadratowego
+
+    :param a: wsplczynnik przy x ** 2
+    :param b: wsplczynnik przy x
+    :param c: wsplczynnik bez x
+    :return:  { ile_pierwiastkow: n, pierwiastki: (0, ..., n-1) }
+    """
     return {"ile_pierwiastkow": 0, "pierwiastki": (0,)}
 
 
