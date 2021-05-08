0c4ed35c2488274e6c872a7473b3252c2caf78e9
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2021-05-08 15:02:31 +0200

    pliki

--- _05_Wyjatki/a01pliki.py
+++ _05_Wyjatki/a01pliki.py
@@ -1,9 +1,8 @@
 # operacje na plikach
 
 f = open('plik.txt', 'w')
-print f
+print(f)
 
-print '-'*20
 
 f.write('1234567890abcdefghijk\n')
 f.write('Ala ma kota\n')
@@ -14,28 +13,26 @@ f.flush()
 f.write('Ola ma kota\n')
 f.close()
 
-print f
-
-print '-'*20
+print(f)
 
 f = open('plik.txt', 'r')
 
-print 'Pocz   : ', f.tell()
+print('Pocz   : ', f.tell())
 s = f.read(10)
-print s
-print 'Read 1 : ', f.tell()
-print f.read()
-print 'Read 2 : ', f.tell()
-f.seek(-15, 2)
-print 'Read 3 : ', f.tell()
-print f.read(3)
-print 'Seek 2 : ', f.tell()
-print f.read()
+print(s)
+print('Read 1 : ', f.tell())
+print(f.read())
+print('Read 2 : ', f.tell())
+f.seek(0, 2)
+print('Read 3 : ', f.tell())
+print(f.read(3))
+print('Seek 2 : ', f.tell())
+print(f.read())
 
 f.close()
 
 f = open(r'a01pliki.py', 'r')
 lista = f.readlines()
-print "".join(lista)
+print("".join(lista))
 
 f.close()
--- _05_Wyjatki/a02pickle.py
+++ _05_Wyjatki/a02pickle.py
@@ -1,107 +1,107 @@
 import pickle
 
-# print dir(pickle)
-#
-# x = 1 + 2j
-# print 'x = ', x
-# s = pickle.dumps(x)
-# print 's = ', s
-# y = pickle.loads(s)
-# print 'x = ', x, 'y = ', y, x is y, x == y
-#
-# y += 1
-# print 'x = ', x, 'y = ', y, x is y, x == y
+print(dir(pickle))
+
+x = 1 + 2j
+print('x = ', x)
+s = pickle.dumps(x)
+print('s = ', s)
+y = pickle.loads(s)
+print('x = ', x, 'y = ', y, x is y, x == y)
+
+y += 1
+print('x = ', x, 'y = ', y, x is y, x == y)
 
 
 # Bardziej skomplikowany przyklad
 
 
-class A:
-    def __init__(self, a=1):
-        self.a = a
-    def __repr__(self):
-        return "A:[ a = "+str(self.a) + " ]"
-
-
-class B(A):
-    def __init__(self, a=2, b=3):
-        A.__init__(self, a)  # trzeba wywolac inicjalizacje przodka
-        self.b = b
-    def __repr__(self):
-        return "B:[ a = "+str(self.a)+", b = "+str(self.b) + " ]"
-
-a = A(11)
-b = B(22, 33)
-print a
-print b
-
-a = A(11)
-b = B(a, 33)
-print a
-print b
-
-bDeep = pickle.dumps(b)
-bb = pickle.loads(bDeep)
-print bb
-
-bb.a.a = -999
-print bb
-print b
-print '='*20
-print pickle.dumps(A)
-print '-'*20
-print pickle.dumps(a)
-print '='*20
-print pickle.dumps(B)
-print '-'*20
-print pickle.dumps(b)
-print '='*20
-
-print pickle.loads(pickle.dumps(b))
-
-
-lista = [1, "Ala", 1+2j, [1, 2]]
-# print pickle.dumps(lista)
-ll = pickle.loads(pickle.dumps(lista))
-print 'll = ', ll, ll is lista, ll == lista
-
-
-class Maz:
-    def __init__(self, zona=1):
-        self.bylem = False
-        self.zona = zona
-    def __repr__(self):
-        s = ""
-        if not self.bylem:
-            self.bylem = True
-            s = "Maz:[ zona = "+str(self.zona) + " ]"
-        else:
-            s = "..."
-        self.bylem = False
-        return s
-
-
-class Zona:
-    def __init__(self, maz=1):
-        self.bylem = False
-        self.maz = maz
-    def __repr__(self):
-        s = ""
-        if not self.bylem:
-            self.bylem = True
-            s = "Zona:[ maz = "+str(self.maz) + " ]"
-        else:
-            s = "..."
-        self.bylem = False
-        return s
-m = Maz()
-z = Zona(m)
-m.zona = z
-
-mm = pickle.dumps(m)
-
-print m is mm, mm == m
-print m
+# class A:
+#     def __init__(self, a=1):
+#         self.a = a
+#     def __repr__(self):
+#         return "A:[ a = "+str(self.a) + " ]"
+#
+#
+# class B(A):
+#     def __init__(self, a=2, b=3):
+#         A.__init__(self, a)  # trzeba wywolac inicjalizacje przodka
+#         self.b = b
+#     def __repr__(self):
+#         return "B:[ a = "+str(self.a)+", b = "+str(self.b) + " ]"
+#
+# a = A(11)
+# b = B(22, 33)
+# print a
+# print b
+#
+# a = A(11)
+# b = B(a, 33)
+# print a
+# print b
+#
+# bDeep = pickle.dumps(b)
+# bb = pickle.loads(bDeep)
+# print bb
+#
+# bb.a.a = -999
+# print bb
+# print b
+# print '='*20
+# print pickle.dumps(A)
+# print '-'*20
+# print pickle.dumps(a)
+# print '='*20
+# print pickle.dumps(B)
+# print '-'*20
+# print pickle.dumps(b)
+# print '='*20
+#
+# print pickle.loads(pickle.dumps(b))
+#
+#
+# lista = [1, "Ala", 1+2j, [1, 2]]
+# # print pickle.dumps(lista)
+# ll = pickle.loads(pickle.dumps(lista))
+# print 'll = ', ll, ll is lista, ll == lista
+#
+#
+# class Maz:
+#     def __init__(self, zona=1):
+#         self.bylem = False
+#         self.zona = zona
+#     def __repr__(self):
+#         s = ""
+#         if not self.bylem:
+#             self.bylem = True
+#             s = "Maz:[ zona = "+str(self.zona) + " ]"
+#         else:
+#             s = "..."
+#         self.bylem = False
+#         return s
+#
+#
+# class Zona:
+#     def __init__(self, maz=1):
+#         self.bylem = False
+#         self.maz = maz
+#     def __repr__(self):
+#         s = ""
+#         if not self.bylem:
+#             self.bylem = True
+#             s = "Zona:[ maz = "+str(self.maz) + " ]"
+#         else:
+#             s = "..."
+#         self.bylem = False
+#         return s
+# m = Maz()
+# z = Zona(m)
+# m.zona = z
+#
+# mm = pickle.dumps(m)
+#
+# print m is mm, mm == m
+# print m
 # x = [1]
 # y = [x, 0]
 # y[1] = y
