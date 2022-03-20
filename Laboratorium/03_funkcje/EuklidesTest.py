d7001a69e79c85228163d2b3fcfa33dfe4931580
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2022-03-20 09:07:18 +0100

    Euklides: sample big numbers

--- Laboratorium/03_funkcje/EuklidesTest.py
+++ Laboratorium/03_funkcje/EuklidesTest.py
@@ -10,3 +10,17 @@ class EuklidesTest(unittest.TestCase):
         actual_value = Euklides.euklides(first_number, second_number)
         # Assert
         self.assertEqual(2, actual_value)
+
+    def test_for_two_big_composed_numbers(self):
+        # Arrange
+        first_prime_number = 64781
+        second_prime_number = 68041
+        third_prime_number = 71429
+        # Act
+        actual_value = Euklides.euklides(
+            first_prime_number * second_prime_number,
+            second_prime_number * third_prime_number
+        )
+        # Assert
+        self.assertEqual(second_prime_number, actual_value)
+
