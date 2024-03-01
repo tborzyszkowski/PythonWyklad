5424faddfc43c97e76f8b9fb9cb25b4fa5808a69
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2024-03-01 10:46:10 +0100

    Epsilon decimal

---
+++ Laboratorium/_01_start/EpsilonDecimal.py
@@ -0,0 +1,14 @@
+from decimal import *
+
+epsilon = Decimal(1.0)
+n = 0
+eps = Decimal(epsilon)
+limit = 1000 * 1000
+
+while epsilon > 0.0 and n < limit:
+    eps = epsilon
+    epsilon /= Decimal(2)
+    n += 1
+
+print("Epsilon 0:", eps)
+print("N:", n)
